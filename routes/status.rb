3195c9b6486a456b407b3c38e560ff2a8724c38c
Jonathan Ballard <jonathan.ballard@gmail.com>
2016-01-04 19:04:32 -0500

    Updating route.

--- routes/status.rb
+++ routes/status.rb
@@ -1,6 +1,6 @@
 class IesApi < Sinatra::Application
   get '/status' do
-    @resp = OpenStruct.new( code: 200 )
-    rabl :status, format: :json
+    @resp = OpenStruct.new( message: "Ok" )
+    rabl :'status/status', format: :json
   end
 end
