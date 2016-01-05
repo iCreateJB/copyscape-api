bf88df958b342105348c4b88ab5235239a39ab0c
Jonathan Ballard <jonathan.ballard@gmail.com>
2016-01-04 19:04:23 -0500

    Adding filters.

--- app.rb
+++ app.rb
@@ -9,7 +9,6 @@ require_relative 'request/init'
 require_relative 'response/init'
 require_relative 'services/init'
 
-
 Rabl.configure do |config|
   config.include_json_root = false
   config.include_child_root = false
@@ -17,25 +16,27 @@ end
 
 Rabl.register!
 
+before do
+  unless request.media_type == 'application/json'
+    halt 415, { error: 'Unsupported media type' }.to_json
+  end
+end
+
 after do
   response.headers['Content-Type'] = 'application/json'
 end
 
 class IesApi < Sinatra::Application
   def parse_params
-    if @env["Content-Type"] == 'application/json'
-      rack_input = @env["rack.input"].read
-      @env["rack.input"].rewind
-      return if rack_input.empty?
-      begin
-        body_content = HashWithIndifferentAccess.new(JSON.parse(URI.decode(rack_input)))
-      rescue JSON::ParserError
-        halt render :json, { status: 400, error: 'Invalid Request'}
-      end
-      @params.merge!(body_content)
-      @env["rack.request.form_hash"] = body_content
-    else
-      halt render :json, { status: 415, error: 'Unsupported media type'}
+    rack_input = @env["rack.input"].read
+    @env["rack.input"].rewind
+    return if rack_input.empty?
+    begin
+      body_content = HashWithIndifferentAccess.new(JSON.parse(URI.decode(rack_input)))
+    rescue JSON::ParserError
+      halt 400, { error: 'Invalid Request' }.to_json
     end
+    @params.merge!(body_content)
+    @env["rack.request.form_hash"] = body_content
   end
 end
